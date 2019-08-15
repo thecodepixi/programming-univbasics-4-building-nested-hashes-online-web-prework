beb2c99779fb81fd2a2f924a235d9b6a6d43235d
Emily Harber <emily.a.harber@gmail.com>
2019-08-15 00:37:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -51,10 +51,10 @@ def monopoly_with_fourth_tier
       four_pieces_owned: 200
       },
     names: {
-    reading_railroad: {mortgage_value:"$100"},
-    pennsylvania_railroad: {mortgage_value:"$100"},
-    b_and_o_railroad: {mortgage_value:"$100"},
-    shortline_railroad: {mortgage_value:"$100"},
+    reading_railroad: {"mortgage_value" => "$100"},
+    pennsylvania_railroad: {"mortgage_value" => "$100"},
+    b_and_o_railroad: {"mortgage_value" => "$100"},
+    shortline_railroad: {"mortgage_value" => "$100"},
     }
   }  
  }
